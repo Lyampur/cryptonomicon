d569ee10dbc6481212c49ee006b671d41bd35d87
Lyampur <lyampurda@gmail.com>
2021-03-17 11:54:57 +0300

    validation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="container mx-auto flex flex-col items-center bg-gray-100 p-4">
+    <div
+      class="fixed w-100 h-100 opacity-80 bg-purple-800 inset-0 z-50 flex items-center justify-center"
+      v-if="!coinlist"
+    >
+      <svg
+        class="animate-spin -ml-1 mr-3 h-12 w-12 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+        ></path>
+      </svg>
+    </div>
     <div class="container">
-      <div class="w-full my-4"></div>
       <section>
         <div class="flex">
           <div class="max-w-xs">
@@ -18,6 +42,22 @@
                 class="block w-full pr-10 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
                 placeholder="Например DOGE"
               />
+            </div>
+            <div
+              class="flex bg-white shadow-md p-1 rounded-md shadow-md flex-wrap"
+              v-if="ticker"
+            >
+              <span
+                class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+                v-for="(t, idx) of recomendedTicker()"
+                :key="idx"
+                @click="add((ticker = t))"
+              >
+                {{ t }}
+              </span>
+            </div>
+            <div class="text-sm text-red-600" v-if="isAdded()">
+              Такой тикер уже добавлен
             </div>
           </div>
         </div>
@@ -139,13 +179,17 @@ export default {
       ticker: "",
       tickers: [],
       selectedTicker: null,
-      graph: []
+      graph: [],
+      coinlist: []
     };
   },
 
   methods: {
     add() {
-      const currentTicker = { name: this.ticker, price: "-" };
+      const currentTicker = { name: this.ticker.toUpperCase(), price: "-" };
+      if (this.isAdded()) {
+        return;
+      }
       this.tickers.push(currentTicker);
       setInterval(async () => {
         const response = await fetch(
@@ -176,7 +220,26 @@ export default {
       return this.graph.map(
         price => 5 + ((price - minValue) * 95) / (maxValue - minValue)
       );
+    },
+
+    isAdded() {
+      return this.tickers.some(t => t.name == this.ticker.toUpperCase());
+    },
+
+    recomendedTicker() {
+      let arr = this.coinlist.filter(coin =>
+        coin.includes(this.ticker.toUpperCase())
+      );
+
+      return arr.slice(0, 4);
     }
+  },
+
+  created() {
+    fetch(`https://min-api.cryptocompare.com/data/all/coinlist?summary=true`)
+      .then(response => response.json())
+      .then(data => (this.coinlist = Object.keys(data.Data)))
+      .catch(error => console.log(error));
   }
 };
 </script>
